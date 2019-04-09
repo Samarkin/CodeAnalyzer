61d3178f48625cf4f623031093cf755e69403f93
Pavel Samarkin <samarkinpa@gmail.com>
2019-04-08 23:14:11 -0700

    Add more languages

--- CodeAnalyzer/CommonLanguage.cpp
+++ CodeAnalyzer/CommonLanguage.cpp
@@ -15,6 +15,9 @@ const Language cPlusPlus{"C/C++", QList<QRegExp>
     {
         regexFromWildcard("*.cpp"),
         regexFromWildcard("*.hpp"),
+        regexFromWildcard("*.ipp"),
+        regexFromWildcard("*.moc"),
+        regexFromWildcard("*.inl"),
         regexFromWildcard("*.cxx"),
         regexFromWildcard("*.c++"),
         regexFromWildcard("*.cp"),
@@ -28,17 +31,66 @@ const Language* const CommonLanguage::CPlusPlus = &cPlusPlus;
 const Language cSharp{"C#", QList<QRegExp>{regexFromWildcard("*.cs")}};
 const Language* const CommonLanguage::CSharp = &cSharp;
 
-const Language makefile{"Makefile", QList<QRegExp>{regexFromWildcard("Makefile")}};
+const Language css{"CSS", QList<QRegExp>{regexFromWildcard("*.css")}};
+const Language* const CommonLanguage::CSS = &css;
+
+const Language go{"Go", QList<QRegExp>{regexFromWildcard("*.go")}};
+const Language* const CommonLanguage::Go = &go;
+
+const Language makefile{"Makefile", QList<QRegExp>
+    {
+        regexFromWildcard("[mM]akefile"),
+        regexFromWildcard("*.mk"),
+        regexFromWildcard("*.mak"),
+    }};
 const Language* const CommonLanguage::Makefile = &makefile;
+
+const Language java{"Java", QList<QRegExp>
+    {
+        regexFromWildcard("*.java"),
+        regexFromWildcard("*.jav"),
+    }};
+const Language* const CommonLanguage::Java = &java;
 
 const Language javascript{"JavaScript", QList<QRegExp>
     {
         regexFromWildcard("*.js"),
+        regexFromWildcard("*.mjs"),
         regexFromWildcard("*.jsx"),
+        regexFromWildcard("*.es"),
         regexFromWildcard("*.es5"),
         regexFromWildcard("*.es6"),
     }};
 const Language* const CommonLanguage::JavaScript = &javascript;
+
+const Language json{"JSON", QList<QRegExp>
+    {
+        regexFromWildcard("*.json"),
+        regexFromWildcard("*.jsonp"),
+    }};
+const Language* const CommonLanguage::Json = &json;
+
+const Language python{"Python", QList<QRegExp>
+    {
+        regexFromWildcard("*.py"),
+        regexFromWildcard("*.pyw"),
+        regexFromWildcard("*.pyi"),
+    }};
+const Language* const CommonLanguage::Python = &python;
+
+const Language swift{"Swift", QList<QRegExp>{regexFromWildcard("*.swift")}};
+const Language* const CommonLanguage::Swift = &swift;
+
+const Language markdown{"Markdown", QList<QRegExp>
+    {
+        regexFromWildcard("*.md"),
+        regexFromWildcard("*.mdwn"),
+        regexFromWildcard("*.markdown"),
+        regexFromWildcard("*.mdown"),
+        regexFromWildcard("*.mkdn"),
+        regexFromWildcard("*.mkd"),
+    }};
+const Language* const CommonLanguage::Markdown = &markdown;
 
 const Language xml{"XML/HTML", QList<QRegExp>
     {
@@ -47,7 +99,12 @@ const Language xml{"XML/HTML", QList<QRegExp>
         regexFromWildcard("*.xaml"),
         regexFromWildcard("*.csproj"),
         regexFromWildcard("*.vbproj"),
+        regexFromWildcard("*.plist"),
+        regexFromWildcard("*.storyboard"),
+        regexFromWildcard("*.xcscheme"),
+        regexFromWildcard("*.xcbkptlist"),
+        regexFromWildcard("*.xcworkspacedata"),
     }};
 const Language* const CommonLanguage::Xml = &xml;
 
-const Language* const CommonLanguage::All[] = {CSharp, CPlusPlus, Makefile, JavaScript, Xml};
+const Language* const CommonLanguage::All[] = {CSharp, CPlusPlus, CSS, Go, Makefile, Java, JavaScript, Json, Python, Swift, Markdown, Xml};
--- CodeAnalyzer/CommonLanguage.h
+++ CodeAnalyzer/CommonLanguage.h
@@ -7,11 +7,18 @@ struct CommonLanguage
 {
     static const Language* const CPlusPlus;
     static const Language* const CSharp;
+    static const Language* const CSS;
+    static const Language* const Go;
     static const Language* const Makefile;
+    static const Language* const Java;
     static const Language* const JavaScript;
+    static const Language* const Json;
+    static const Language* const Python;
+    static const Language* const Swift;
+    static const Language* const Markdown;
     static const Language* const Xml;
 
-    static const Language* const All[5];
+    static const Language* const All[12];
 };
 
 #endif // COMMONLANGUAGES_H
